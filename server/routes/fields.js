f9698bdf294ac39916fb477a9bd41aa2814b1dd8
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-04 22:50:47 -0400

    refactored routes

--- server/routes/fields.js
+++ server/routes/fields.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { getFields } from '../controllers/fields.js'
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send('This Works')
-});
+router.get("/", getFields)
 
 export default router

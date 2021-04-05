79f943bb055b89268e167a6446b6851e6ad9b873
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-04 21:23:48 -0400

    created Field model

---
+++ server/controllers/fields.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+
+export const getFields = async (res, req) => {
+    try{
+        
+    } catch {
+
+    }
+}
---
+++ server/models/field.js
@@ -0,0 +1,23 @@
+const { Mongoose } = require("mongoose");
+import mongoose from 'mongoose'
+
+
+const fieldSchema = new mongoose.Schema({
+
+    name: String, 
+    location: String, 
+    cost: {
+        type: Number, 
+        default: 0
+    }, 
+    createdAt: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+const Field = mongoose.model('Field', fieldSchema)
+
+export default Field
+
+

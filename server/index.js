9ef2ca3110eb9ecadc74a19557057d6fef14df1f
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-04 20:55:50 -0400

    server is running

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => app.listen(PORT, () => console.log(`Server running on: ${PORT}`)))
+  .catch((error) => console.log(error));
+
+mongoose.set("useFindAndModify", false);

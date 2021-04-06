375be548396f0b5a873a25cca192fbbf6349fcb1
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-05 23:57:06 -0400

    created navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import NavBar from './Components/Containers/NavBar'
 import MapArea from './Components/Map/MapArea'
-import Map from './Components/Map/MapArea'
 
 
 const App = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <MapArea></MapArea>
         </div>
     )
---
+++ client/src/Components/Containers/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import AppBar from '@material-ui/core'
+
+
+export default function NavBar () {
+
+
+    return (
+        <AppBar position='static'></AppBar>
+    )
+
+}

0ad6b48713a6abca73d04519ede2c5990be571c7
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-06 22:09:43 -0400

    updated styles

--- client/src/Components/Containers/NavBar.js
+++ client/src/Components/Containers/NavBar.js
@@ -1,14 +1,31 @@
-import React from 'react'
-import { fade, makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 
-import AppBar from '@material-ui/core/AppBar'
-import useStyles from "./styles"
 
-export default function NavBar () {
-    const classes = useStyles()
 
-    return (
-        <AppBar className={classes.navBar} position='static'>BAr</AppBar>
-    )
+import AppBar from "@material-ui/core/AppBar";
+import useStyles from "./styles";
 
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar className={classes.navBar} position="static">
+      <div className={classes.search}>
+        <div className={classes.searchIcon}>
+          <SearchIcon />
+        </div>
+        <InputBase
+          placeholder="Search…"
+          classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+          }}
+          inputProps={{ "aria-label": "search" }}
+        />
+      </div>
+    </AppBar>
+  );
 }
--- client/src/Components/Map/MapArea.js
+++ client/src/Components/Map/MapArea.js
@@ -1,49 +1,54 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const containerStyle = {
-  width: '1600px',
-  height: '600px'
+  width: "995px",
+  height: "600px",
 };
 
 const center = {
   lat: -3.745,
-  lng: -38.523
+  lng: -38.523,
 };
 
-  const MapArea = () => {
-    const { isLoaded } = useJsApiLoader({
-      id: 'google-map-script',
-      googleMapsApiKey: process.env.REACT_APP_GOOGLE_API
-    })
-  
-    const [map, setMap] = React.useState(null)
-  
-    const onLoad = React.useCallback(function callback(map) {
-      const bounds = new window.google.maps.LatLngBounds();
-      map.fitBounds(bounds);
-      setMap(map)
-    }, [])
-  
-    const onUnmount = React.useCallback(function callback(map) {
-      setMap(null)
-    }, [])
-  
-    return isLoaded ? (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={5}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
-    ) : <></>
-  }
+const q = "seattle"
 
-export default MapArea
+const MapArea = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API,
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      // center={center}
+      q={q}
+      zoom={5}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+};
+
+export default MapArea;

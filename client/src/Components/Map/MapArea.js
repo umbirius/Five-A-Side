fc39c030bbd2e5855ef8a2be8ba754840b080bba
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-11 11:07:49 -0400

    refactored API map, updated style

--- client/src/Components/Map/MapArea.js
+++ client/src/Components/Map/MapArea.js
@@ -1,55 +1,50 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import dotenv from "dotenv";
-import useStyles from "./styles";
-import Geolocation from "react-native-geolocation-service";
+import styles from "./styles";
 
 dotenv.config();
 
-const center = {
-  lat: 40.728157,
-  lng: -74.077644,
-};
+const libraries = ["places"];
 
 const mapContainerStyle = {
   width: "100%",
   height: "800px",
 };
 
-const MapArea = () => {
-  const classes = useStyles();
+const center = {
+  lat: 40.728157,
+  lng: -74.077644,
+};
 
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
+const options = {
+  styles: styles
+}
+
+const MapArea = () => {
+
+  // loads google script, gives back two variables (isLoaded and loadError)
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API,
+    // enables places libraries when google script loads
+    libraries,
   });
 
-  const [map, setMap] = useState(null);
+  if (loadError) return "Error Loading Maps";
+  if (!isLoaded) return "Loading Maps";
 
-  const onLoad = useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
-
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
-  return isLoaded ? (
+  return (
     <GoogleMap
-      className={classes.mapArea}
       mapContainerStyle={mapContainerStyle}
+      zoom={10}
       center={center}
-      zoom={5}
-      onLoad={onLoad} //useEffect goes here?
-      onUnmount={onUnmount} //useEffect goes here?
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
+      options={options}></GoogleMap>
   );
 };
 

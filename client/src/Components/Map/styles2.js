4fd33fa10ad955f58fe1ac789aea80074d435135
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-12 23:36:10 -0400

    added locate

--- client/src/Components/Map/styles2.js
+++ client/src/Components/Map/styles2.js
@@ -1,15 +1,22 @@
-import { makeStyles } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core";
 
-export default makeStyles( () => ({
+export default makeStyles(() => ({
+  search: {
+    // position: "absolute",
+    top: "1rem",
+    left: "50%",
+    transform: "translateX(-50%)",
+    width: "100%",
+    maxWidth: "400px",
+    zIndex: "10",
+  },
 
-    search: {
-        position: "absolute",
-        top: "1rem", 
-        left: "50%",
-        transform: "translateX(-50%)",
-        width: "100%",
-        maxWidth: "400px", 
-        zIndex: "10",
-    }
-
-}))
+  locate: {
+    position: "absolute",
+    top: "1rem",
+    right: "1rem",
+    background: "none",
+    border: "none",
+    zIndex: "10",
+  },
+}));

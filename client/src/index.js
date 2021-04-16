64053ffb9b295cbbd11e5fc309d136eaa1bfce17
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-15 21:53:02 -0400

    composed store with dev tools

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>

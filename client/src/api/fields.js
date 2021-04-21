7fe91fd4e1593ee7a1c1213885d8b22d4dc7f7d1
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-20 23:27:05 -0400

    added createField

--- client/src/api/fields.js
+++ client/src/api/fields.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 const url = 'http://localhost:5000/fields'
 
 export const fetchFields = () => axios.get(url)
+export const createField = () => (newField) => axios.post(url, newField)

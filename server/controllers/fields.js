26d8b1da74a6698898f7ad85be3122c8e95504c9
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-20 23:24:25 -0400

    added createField

--- server/controllers/fields.js
+++ server/controllers/fields.js
@@ -3,10 +3,24 @@ import Field from "../models/field.js";
 
 export const getFields = async (req, res) => {
   try {
-    const Fields = await Field.find();
+    const fields = await Field.find();
 
-    res.status(200).json(Fields);
+    res.status(200).json(fields);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
+};
+
+export const createField = async (req, res) => {
+  const field = req.body;
+  const newField = new Field(field);
+  try {
+    await newField.save();
+    res.status(201).json(newField);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+
+  res.send("Field created")
+
 };

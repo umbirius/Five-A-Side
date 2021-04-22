4a64a818292f206594c9638f750aa65e18f6a437
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-21 22:23:50 -0400

    edited style on form

--- client/src/Components/Form/styles.js
+++ client/src/Components/Form/styles.js
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 
 export default makeStyles(() => ({
-  newField: {
+  form: {
     zIndex: "12",
     position: "absolute",
     top: "25%",
@@ -9,6 +9,26 @@ export default makeStyles(() => ({
     right: "30%",
     justifyContent: "center",
     alignItems: "center",
-    display:"flex"
+    display: "flex",
+    border: "",
   },
+
+  button: {
+      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      padding: "2px",
+      verticalAlign: "center",
+      alignItems: "center", 
+      margin:"3px",
+      width: "50%", 
+      justifyContent: "center"
+  },
+
+  inputField: {
+    width: "60%",
+    margin: "10px"
+  },
+
+  header: {
+      
+  }
 }));

d076a22674c76f227ae3250281b5334b6d6d7618
Umberto Palazzo <upalazzo93@gmail.com>
2021-04-21 22:24:10 -0400

    filled out onchange

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // import actions to create a field here
 // import reducer methods from react-redux
@@ -10,16 +10,57 @@ const Form = () => {
   //   const [fieldData, setFieldData] = useState({});
   const classes = useStyles();
   const [newField, setNew] = useState(false);
+  const [fieldData, setFieldData] = useState({
+    name: "",
+    cost: "",
+    rating: "",
+    location: "",
+  });
+
+  useEffect(() => {
+    console.log(fieldData);
+  });
+
+  const clear = () => {
+    setFieldData({ name: "", cost: "", rating: "", location: "" });
+  };
 
   return (
-    <Paper className={classes.newField}>
-      <form>
+    <Paper className={classes.form} elevation={10}>
+      <form autoComplete="off">
         <Typography>Create New Field</Typography>
-        <TextField name="name" label="Name"></TextField>
-        <TextField name="cost" label="Cost"></TextField>
-        <TextField name="rating" label="Rating"></TextField>
-        <TextField name="location" label="Location"></TextField>
+        <TextField
+          className={classes.inputField}
+          name="name"
+          label="Name"
+          value={fieldData.name}
+          onChange={(e) => setFieldData({ ...fieldData, name: e.target.value })}
+        ></TextField>
+        <TextField
+          className={classes.inputField}
+          name="cost"
+          label="Cost"
+          value={fieldData.cost}
+          onChange={(e) => setFieldData({ ...fieldData, cost: e.target.value })}
+        ></TextField>
+        <TextField
+          className={classes.inputField}
+          name="rating"
+          label="Rating"
+          value={fieldData.rating}
+          onChange={(e) =>
+            setFieldData({ ...fieldData, rating: e.target.value })
+          }
+        ></TextField>
+        <TextField
+          className={classes.inputField}
+          name="location"
+          label="Location"
+          value={fieldData.location}
+        ></TextField>
         <Button
+          className={classes.button}
+          variant="contained"
           onClick={() => {
             console.log("create new field");
           }}
@@ -28,12 +69,24 @@ const Form = () => {
           Submit
         </Button>
         <Button
+          className={classes.button}
+          variant="contained"
           onClick={() => {
             console.log("close form");
           }}
         >
           {" "}
           Cancel
+        </Button>
+        <Button
+          className={classes.button}
+          variant="contained"
+          onClick={() => {
+            clear();
+          }}
+        >
+          {" "}
+          Clear Form
         </Button>
       </form>
     </Paper>

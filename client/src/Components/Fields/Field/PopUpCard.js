85a8ede22a644d21868675896b0f9e6d8224b629
Umberto Palazzo <upalazzo93@gmail.com>
2021-05-01 20:26:13 -0400

    created popUpCard

---
+++ client/src/Components/Fields/Field/PopUpCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography,
+  } from "@material-ui/core";
+
+
+const PopUpCard = ({field}) => {
+
+    return (
+        <Card key={field.id}>
+            <CardMedia>
+
+            </CardMedia>
+        </Card>
+    )
+}
